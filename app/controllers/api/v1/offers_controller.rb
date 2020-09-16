0f320e7b70bb7b9f202eed82eb0af806ca1284e0
ritaly <malgorzata.lyczywek@gmail.com>
2020-09-16 15:59:09 +0200

    accepts_nested_attributes_for offer

--- app/controllers/api/v1/offers_controller.rb
+++ app/controllers/api/v1/offers_controller.rb
@@ -3,20 +3,21 @@
 module Api
   module V1
     class OffersController < ApplicationController
-      # GET /offers
+      # GET api/v1/offers
       def index
         offers = Offer.all
-        # options = { include: [:orders] }
+        #options = { include: [:orders] }
         #render json: OfferSerializer.new(offers, options).serialized_json
         render json: offers, include: :orders, status: :ok
       end
 
-      # GET /offers/1
+      # GET api/v1/offers/:id
       def show
         offer = Offer.find(params[:id])
         render json: OfferSerializer.new(offer).serialized_json
       end
 
+      # POST api/v1/offers
       def create
         offer = Offer.new(offer_params)
         if offer.save
@@ -31,7 +32,7 @@ module Api
       # Trusted parameters through
       def offer_params
         params.require(:offer).permit(:status, :client_id, :salesman_id,
-                                      orders: %i[quantity product_id])
+                                      orders_attributes: %w[quantity product_id])
       end
     end
   end
--- app/models/offer.rb
+++ app/models/offer.rb
@@ -5,4 +5,7 @@ class Offer < ApplicationRecord
   has_many :orders
   belongs_to :client
   belongs_to :salesman
+  accepts_nested_attributes_for :orders
+
+  validates :status, inclusion: { in: %w[accepted rejected] }, allow_blank: true
 end
